e9438b8a84e843a34fccd6af947271a520a46a2a
knoff <nixos-git@knoc.one>
2023-02-17 12:40:33 +0100

    initial commit

---
+++ default.nix
@@ -0,0 +1,31 @@
+# allow our nixpkgs import to be overridden if desired
+{ pkgs ? import <nixpkgs> {} }:
+
+# let's write an actual basic derivation
+# this uses the standard nixpkgs mkDerivation function
+pkgs.stdenv.mkDerivation {
+
+  # name of our derivation
+  name = "basic-derivation";
+
+  # sources that will be used for our derivation.
+  src = ./src;
+  # fetchURL/ Git
+  # 
+
+  # see https://nixos.org/nixpkgs/manual/#ssec-install-phase
+  # $src is defined as the location of our `src` attribute above
+  installPhase = ''
+    # $out is an automatically generated filepath by nix,
+    # but it's up to you to make it what you need. We'll create a directory at
+    # that filepath, then copy our sources into it.
+    mkdir $out
+    mkdir $out/mods
+    
+    ferium --config-file $src/config.json upgrade
+
+    fabric-installer
+    
+    cp -rv $src/* $out
+  '';
+}
